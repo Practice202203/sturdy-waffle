535b6843ebcc36a064b4c6fb1f216c70cd1dae26
Ruslan <ya4pledovo@yandex.ru>
2022-03-22 14:15:59 +0500

    Final version w/out comments

--- literate-broccoli-master-master/js/catalog_icon.js
+++ literate-broccoli-master-master/js/catalog_icon.js
@@ -3,10 +3,10 @@ var catalog_icon = document.querySelectorAll('.catalog_icon_i');
 
 catalog.addEventListener("mouseenter", function() {
 
-    catalog_icon[0].style.transform = "translateY(5px) translateZ(0) rotate(45deg)";
+    catalog_icon[0].style.transform = "translateY(7px) translateZ(0) rotate(45deg)";
     catalog_icon[1].style.transition = "opacity .3s;";
     catalog_icon[1].style.opacity = 0;
-    catalog_icon[2].style.transform = "translateY(-5px) translateZ(0) rotate(-45deg)";
+    catalog_icon[2].style.transform = "translateY(-7px) translateZ(0) rotate(-45deg)";
 })
 
 catalog.addEventListener("mouseleave", function() {
